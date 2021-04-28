743d1b7b1a6c41651f5d3acfe4d1b917724fe9c9
StinaLue <43956910+StinaLue@users.noreply.github.com>
2021-04-28 23:32:15 +0200

    histogram simple and efficace

--- histogram.py
+++ histogram.py
@@ -11,26 +11,15 @@ Histogram which shows which Hogwarts course has a homogeneous score distribution
 
 df = pd.read_csv("./dataset_train.csv")
 
-#hufflepuffs = df[df['Hogwarts House' == 'Hufflepuff']]
-
 color_dict = dict({'Hufflepuff':'gold',
                   'Slytherin':'lime',
                   'Ravenclaw': 'blue',
                   'Gryffindor': 'red'})
 #sns.set_theme()
-fig, axes = plt.subplots(3, 5, figsize=(18, 10), sharey=True)
-sns.histplot(ax=axes[0, 0], data=df, x="Herbology", hue="Hogwarts House", palette=color_dict, element="step", bins=15)#,kde=True
-sns.histplot(ax=axes[0, 1], data=df, x="Flying", hue="Hogwarts House", palette=color_dict, element="step", bins=15)#,kde=True
-sns.histplot(ax=axes[0, 2], data=df, x="Arithmancy", hue="Hogwarts House", palette=color_dict, element="step", bins=15)#,kde=True
-sns.histplot(ax=axes[0, 3], data=df, x="Astronomy", hue="Hogwarts House", palette=color_dict, element="step", bins=15)#,kde=True
-sns.histplot(ax=axes[1, 0], data=df, x="Defense Against the Dark Arts", hue="Hogwarts House", palette=color_dict, element="step", bins=15)#,kde=True
-sns.histplot(ax=axes[1, 1], data=df, x="Divination", hue="Hogwarts House", palette=color_dict, element="step", bins=15)#,kde=True
-sns.histplot(ax=axes[1, 2], data=df, x="Muggle Studies", hue="Hogwarts House", palette=color_dict, element="step", bins=15)#,kde=True
-sns.histplot(ax=axes[1, 3], data=df, x="Ancient Runes", hue="Hogwarts House", palette=color_dict, element="step", bins=15)#,kde=True
-sns.histplot(ax=axes[2, 0], data=df, x="History of Magic", hue="Hogwarts House", palette=color_dict, element="step", bins=15)#,kde=True
-sns.histplot(ax=axes[2, 1], data=df, x="Transfiguration", hue="Hogwarts House", palette=color_dict, element="step", bins=15)#,kde=True
-sns.histplot(ax=axes[2, 2], data=df, x="Charms", hue="Hogwarts House", palette=color_dict, element="step", bins=15)#,kde=True
-sns.histplot(ax=axes[2, 3], data=df, x="Potions", hue="Hogwarts House", palette=color_dict, element="step", bins=15)#,kde=True
-sns.histplot(ax=axes[1, 4], data=df, x="Care of Magical Creatures", hue="Hogwarts House", palette=color_dict, element="step", bins=15)#,kde=True
-
+nrows = 3
+ncolumns = 5
+fig, axes = plt.subplots(nrows, ncolumns, figsize=(18, 10), sharey=True, squeeze=True)
+for i,feature in enumerate(df.columns[6:]):
+    sns.histplot(ax=axes[int(i / ncolumns), int(i % ncolumns)], data=df, x=feature, hue="Hogwarts House", palette=color_dict, element="step", bins=15)
+    #fig.delaxes(axes.flatten()[i])
 plt.show()

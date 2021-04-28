78ba095a4123aca5d9341aed7a9f00810495892b
Antoine Fonck <antoinefonck98@gmail.com>
2021-04-29 00:27:06 +0200

    added scatter plot

--- histogram.py
+++ histogram.py
@@ -15,11 +15,14 @@ color_dict = dict({'Hufflepuff':'gold',
                   'Slytherin':'lime',
                   'Ravenclaw': 'blue',
                   'Gryffindor': 'red'})
-#sns.set_theme()
+sns.set_theme()
 nrows = 3
 ncolumns = 5
-fig, axes = plt.subplots(nrows, ncolumns, figsize=(18, 10), sharey=True, squeeze=True)
+fig, axes = plt.subplots(nrows, ncolumns, figsize=(18, 10), sharey=True)#, squeeze=True)
 for i,feature in enumerate(df.columns[6:]):
-    sns.histplot(ax=axes[int(i / ncolumns), int(i % ncolumns)], data=df, x=feature, hue="Hogwarts House", palette=color_dict, element="step", bins=15)
+    sns.histplot(ax=axes[int(i / ncolumns), int(i % ncolumns)], data=df, x=feature, hue="Hogwarts House", palette=color_dict, element="step", bins=15)#, legend=False)
     #fig.delaxes(axes.flatten()[i])
+
+#fig.tight_layout()
+#plt.tight_layout()
 plt.show()
--- scatter_plot.py
+++ scatter_plot.py
@@ -6,7 +6,7 @@ import numpy as np
 import seaborn as sns
 
 """
-Histogram which shows which Hogwarts course has a homogeneous score distribution between all four houses
+Pairplot which shows which Hogwarts courses are the most similar
 """
 
 df = pd.read_csv("./dataset_train.csv")
@@ -15,10 +15,7 @@ color_dict = dict({'Hufflepuff':'gold',
                   'Slytherin':'lime',
                   'Ravenclaw': 'blue',
                   'Gryffindor': 'red'})
-#sns.set_theme()
-nrows = 3
-ncolumns = 5
-fig, axes = plt.subplots(nrows, ncolumns, figsize=(18, 10), sharey=True, squeeze=True)
-for i,feature in enumerate(df.columns[6:]):
-    sns.scatterplot(ax=axes[int(i / ncolumns), int(i % ncolumns)], data=df, hue="Hogwarts House", y="Flying", x=feature, palette=color_dict)
+sns.set_theme()
+g = sns.pairplot(df.drop("Index", axis=1), hue="Hogwarts House", palette=color_dict, dropna=True)
+g.savefig("pairplot.png")
 plt.show()
